f37e7b44a3f47a568e3782a6ce8605a77ad1ae59
Jose Salazar <solisjose466@gmail.com>
2022-09-17 06:24:22 +0000

    permanent sessions implemented!

--- backend/server/application.py
+++ backend/server/application.py
@@ -1,21 +1,21 @@
+import imp
 from constants import *
 from db_creds import *
-from flask import Flask, redirect, url_for, request, send_from_directory, make_response
-from flask_session import Session
+from flask import Flask, redirect, url_for, request, send_from_directory, make_response, session
 import mysql.connector
 from flask_cors import CORS
+import datetime
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["SECRET_KEY"] = SECRET_KEY["key"] 
+# session lives even after user closes browser
+app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=5)
+
 connection = mysql.connector.connect(host=DB_CREDS['host'], user=DB_CREDS['username'], passwd=DB_CREDS['password'], database=DB_CREDS['database'])
 
 db_cursor = connection.cursor()
-
-# Session management config
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 def is_valid_user(username, password):
     statement = f"select password from users where username='{username}'"
@@ -32,12 +32,29 @@ def is_valid_user(username, password):
     
     return True
 
+
 @app.route('/', methods=['GET'])
 def index():
+    # if not session; redirect to get /login
+    if "user" not in session:
+        return redirect(url_for("get_login"))
+    
+    #else serve index
     return send_from_directory(HTML_PATH["html"], "index.html")
+
+@app.route('/logout', methods=['GET'])
+def get_logout():
+    if "user" in session:
+        session.pop("user", None)
+
+    return redirect(url_for("get_login"))
 
 @app.route('/login', methods=['GET', 'POST'])
 def get_login():
+
+    # user is logged in
+    if "user" in session:
+        return redirect(url_for("index"))
 
     # This is purely for development. Your wsgi server should serve static files.
     if request.method == "GET":
@@ -60,6 +77,9 @@ def get_login():
         return make_response(send_from_directory(HTML_PATH["html"], "login.html"), 401)
     
     # good login
+    # get user a session and return index
+    session.permanent = True
+    session["user"] = username 
     return send_from_directory(HTML_PATH["html"], "index.html")
 
 if __name__ == '__main__':
